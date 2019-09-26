fb62312d715c3c5fa379632a95a4b490e82fc749
caitlining <caitlincing@gmail.com>
2019-09-26 22:21:23 +0000

    Added request library, so package files added for that, also created index.js and iss.js with completed code for the first step of the assignment, ISS Spotter I

---
+++ iss.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+//this will contain most of the logic for fetching the data from each API endpoint
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ * - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ * - An error, if any (nullable)
+ * - the IP address as a string (null if error). Ex; "162.245.144.188"
+*/
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const myIPObj = JSON.parse(body);
+    const myIP = myIPObj.ip;
+    callback(null, myIP);
+  });
+};
+
+module.exports = { fetchMyIP };
